31b9ec4121d8bee7de68cb76228ff8e25b2ea0d2
vikk777 <listratenko1998@mail.ru>
2020-03-24 17:12:18 +0300

    Created base project structure

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-app_dir = os.path.abspath(os.path.dirname(__file__))
+appDir = os.path.abspath(os.path.dirname(__file__))
 
-class BaseConfig():
-	"""Application config"""
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
-	
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or \
+	'mysql+pymysql://root:@localhost/test'
