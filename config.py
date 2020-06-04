d484d76ccb9675d834e00c148045edcb8720ac35
Sherifffox <popovaviktoriaa@gmail.com>
2020-06-05 01:32:16 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ appDir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or \
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or \
+#     'mysql+pymysql://root:@localhost/smart_dict'
+SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL') or \
     'mysql+pymysql://root:@localhost/smart_dict'
 SQLALCHEMY_ENGINE_OPTIONS = {  # 'pool_recycle': 280,
     #'pool_timeout': 100,
